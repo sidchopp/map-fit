2cddfeb63a37319174c37a4aaea4a5b6eeb2a95a
Sid <sidchopp@gmail.com>
2021-08-13 13:21:02 -0600

    leaflet is used in html and js

--- script.js
+++ script.js
@@ -22,6 +22,21 @@ if (navigator.geolocation)
     const { longitude } = position.coords;
     console.log(`https://www.google.ca/maps/@${latitude},${longitude}`);
 
+    ////// code(below) copied form the overview of leaflet library webpage
+    // this 'map' below after L( which is for Leaflet) is the name of our id in the html where we want to show the location in map
+
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+    /////// copied code from leaflet (above)
+
   }, function () {
     alert(' could not locate your position')
   });
